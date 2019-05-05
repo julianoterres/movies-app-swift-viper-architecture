bb9d1fdc9290582ce37dfefcb43683b107aae473
Juliano Terres <juliano@julianoterres.com.br>
2019-05-05 16:26:14 -0300

    Add protocol to detail router

--- Movies/Modules/MovieDetail/MovieDetailProtocols.swift
+++ Movies/Modules/MovieDetail/MovieDetailProtocols.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2019 Juliano Terres. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+// MARK: Methods of MovieDetailRouterProtocol
+protocol MovieDetailRouterProtocol: class {
+  func build(movie: MovieListPresentation) -> UIViewController
+}
--- Movies/Modules/MovieDetail/MovieDetailRouter.swift
+++ Movies/Modules/MovieDetail/MovieDetailRouter.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 // MARK: Methods of MovieDetailRouterProtocol
-class MovieDetailRouter {
+class MovieDetailRouter: MovieDetailRouterProtocol {
   
   func build(movie: MovieListPresentation) -> UIViewController {
     let view = MovieDetailView()
